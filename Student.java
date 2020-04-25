d93e4e326466735d0c9735147094c9d9dfa2b127
SylwiaKantor <44203473+SylwiaKantor@users.noreply.github.com>
2020-04-25 19:52:19 +0200

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,35 @@
+public class Student {
+    private String imie;
+    private String nazwisko;
+    private int nrIndeksu;
+
+    public Student(String imie, String nazwisko, int nrIndeksu) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.nrIndeksu = nrIndeksu;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public int getNrIndeksu() {
+        return nrIndeksu;
+    }
+
+    public void setNrIndeksu(int nrIndeksu) {
+        this.nrIndeksu = nrIndeksu;
+    }
+}
